2cb7e4ca3d629b716452a6799fe8a8c8aae883ac
tdrevitchvgrdn <110933291+timdrevitch@users.noreply.github.com>
2022-09-05 22:32:06 -0400

    Update CreateLoan.tsx

--- client/src/Components/CreateLoan.tsx
+++ client/src/Components/CreateLoan.tsx
@@ -1,5 +1,5 @@
 import axios from "axios"
-import { FC, useContext, useState } from "react"
+import { FC, ChangeEvent, useContext, useState } from "react"
 import { Context } from "../Context"
 
 const CreateLoan: FC = () => {
@@ -20,13 +20,13 @@ const CreateLoan: FC = () => {
     }
 
     //Change state on input field changes
-    const changeLoan = (e: React.ChangeEvent<HTMLInputElement>) => setLoanName(e.target.value)
+    const changeLoan = (e: ChangeEvent<HTMLInputElement>) => setLoanName(e.target.value)
 
     return (
         <div style={{backgroundColor: "#1a1d22", border: "1px solid black", borderRadius: "10px", margin: "5rem 10px 0 10px"}}>
             <form onSubmit={createNewLoan}>
-                <label>Loan: </label>
-                <input type="text" name="loan" value={loanName} onChange={changeLoan} required/>
+                <label htmlFor="loan">Loan name: </label>
+                <input type="text" id="loan" name="loan" value={loanName} onChange={changeLoan} required/>
                 <br/>
                 <button onClick={() => setIfCreationFormIsOpen(false)} style={{cursor: "pointer", display: "inline-block", backgroundColor: "darkred", color: "white", border: "none", borderRadius: "3px", padding: ".5rem 1rem", margin: "1rem", fontFamily: "Trebuchet MS, sans-serif"}}>Cancel</button>
                 <input type="submit" value="Submit new task" style={{cursor: "pointer", display: "inline-block", backgroundColor: "green", color: "white", border: "none", borderRadius: "3px", padding: ".5rem 1rem", margin: "1rem", fontFamily: "Trebuchet MS, sans-serif"}}/>
